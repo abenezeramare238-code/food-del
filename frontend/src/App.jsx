40c58de9a4e6682f70bf64d93eb7f564036d76ab
Abeni <abenezeramare238@gmail.com>
2026-02-07 18:09:28 +0300

    at the end of the home page akebabi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
+import Home from './pages/Home/Home'
 import Cart from './pages/cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import StoreContextProvider from "./context/storeContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StoreContextProvider>
       <App />
-    </BrowserRouter>
-
-)
+    </StoreContextProvider>
+  </BrowserRouter>,
+);
