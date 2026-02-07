c5928accf72be3166c4d265984aa2eb7df05a439
Abeni <abenezeramare238@gmail.com>
2026-02-07 12:37:45 +0300

    upto home page heading background

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,11 @@
-import "./logo.png";
+import reactLogo from './react.svg'
+import logo from './logo.png'
+import search_icon from './search_icon.png'
+import basket_icon from './basket_icon.png'
 
-export const assets = {
-    logo
+export const assets ={
+    reactLogo,
+    logo,
+    search_icon,
+    basket_icon
 }
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'> 
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Choose from diverse menu featuring a delectable arry of dishes crafted with the finest ingideiants and culinary expretise.one delicious meal at atime.</p>
+        <button>View menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
